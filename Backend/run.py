c7ff34c8b3b3d675bb700d325d8b6a8db62efd7d
Robin Sajan <robinsajan4#gmail.com>
2026-04-09 17:28:41 +0530

    added  urls

--- Backend/run.py
+++ Backend/run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False, port=8000)  # Use port 8000 instead
+    port = int(os.environ.get("PORT", 8000))
+    app.run(debug=False, host="0.0.0.0", port=port)
